4f3e4e854879f24fe9690a201a1e86a85f4159e5
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-12-04 15:19:36 +0200

    [features-start] collision completed / game base completed

--- js/app.js
+++ js/app.js
@@ -1,23 +1,44 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+function Creature(sprite, x, y) {
+    this.sprite = sprite;
+    this.x = x;
+    this.y = y;
+}
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+function Enemy(x, y, speed) {
+    Creature.call(this, "images/enemy-bug.png", x, y);
+    this.speed = speed;
+    this.width = 60;
+    this.heigth = 20;
+}
+Enemy.prototype = Object.create(Creature.prototype)
+Object.defineProperty(Enemy.prototype, "constructor", {
+    value: Enemy,
+    enumerable: false,
+    writable: true,
+});
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    if (this.x > random(500, 700)) {
+        this.x = random(-500, -100);
+    }
+    if (
+        this.x + this.width > player.x &&
+        player.x > this.x - this.width &&
+        this.y + this.heigth > player.y &&
+        player.y > this.y - this.heigth
+    ) {
+        player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -25,22 +46,74 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+function Player(x, y) {
+    this.sprite = "images/char-boy.png";
+    this.x = x;
+    this.y = y;
+    this.velX = 100;
+    this.velY = 80;
+}
+
+Player.prototype.update = function () {
+    //
+};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function (e) {
+    if (e === "left") {
+        if (this.x - this.velX > -10) {
+            this.x -= this.velX;
+        }
+    } else if (e === "up") {
+        if (this.y - this.velY > -30) {
+            this.y -= this.velY;
+        }
+        if (this.y < 0) {
+            player.reset();
+        }
+    } else if (e === "right") {
+        if (this.x + this.velX < 500) {
+            this.x += this.velX;
+        }
+    } else if (e === "down") {
+        if (this.y + this.velY < 400) {
+            this.y += this.velY;
+        }
+    }
+};
+
+Player.prototype.reset = function () {
+    this.x = 200;
+    this.y = 380;
+};
+
+function random(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const allEnemies = [];
+while (allEnemies.length < 6) {
+    const enemyTop = new Enemy(random(-500,-100), 60, random(120,500));
+    const enemyMiddle = new Enemy(random(-500, -100), 140, random(120, 500));
+    const enemyBottom = new Enemy(random(-500,-100), 220, random(120,500));
+    allEnemies.push(enemyTop, enemyMiddle, enemyBottom);
+}
+const player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function (e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        KeyA: "left",
+        KeyW: "up",
+        KeyD: "right",
+        KeyS: "down",
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.code]);
 });
